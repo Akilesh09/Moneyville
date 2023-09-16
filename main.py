4e70cb4928625eb25fe5c6815922a6ad8f1a808d
SauravBanerjee04 <78271068+SauravBanerjee04@users.noreply.github.com>
2023-09-16 09:41:04 -0400

    changes to main.py

--- main.py
+++ main.py
@@ -21,18 +21,23 @@ onmap = True # Starts as false
 def draw():
     draw_background()
     if onmap:
-        draw_character
+        draw_character()
+    else: #draw options into four box screen
 
-def draw_character(x,yf):
+def draw_character(x,y):
     WIN.fill(BACKGROUND_COLOR)
-    WIN.blit(MAIN_CHARACTER,(500,300))
+    WIN.blit(MAIN_CHARACTER,(x,y))
     pygame.display.update()
 
-def draw_background():
+def draw_background(): #draws the background, if we're on the map it draws the map otherwise it draws the boxes for the selection screen
     if onmap:
         WIN.fill(BACKGROUND_COLOR)
         WIN.blit(BACKGROUND_MAP,(0,0))
+    else: #draw a box with four descriptions of options
+
     pygame.display.update()
+
+def islegalarea(): 
 
 
 
