09e6c42a0b59d492718f424effb6fbd859cd42c8
Akilesh Mahesh <akilesh.mahesh07@gmail.com>
2023-09-16 04:12:49 -0400

    Add main character

--- main.py
+++ main.py
@@ -1,15 +1,30 @@
 import pygame
+import os
 
 WIDTH,HEIGHT = 1000,600
 WIN = pygame.display.set_mode((WIDTH,HEIGHT))
 pygame.display.set_caption("Moneyville")
+
+BACKGROUND_COLOR = (255,255,255)
+FPS = 60
+
+MAIN_CHARACTER = pygame.transform.scale(pygame.image.load(os.path.join('Assets','main_character.jpg')),(100,70))
+
+def draw_window():
+    WIN.fill(BACKGROUND_COLOR)
+    WIN.blit(MAIN_CHARACTER,(500,300))
+    pygame.display.update()
+
+
 def main():
+    clock = pygame.time.Clock()
     run = True
     while run:
+        clock.tick(FPS)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
-    
+        draw_window()
     pygame.quit()
 
 if __name__ == "__main__":
