40ac41fc0474cf49b7b193c7cbfe4a56c2f6c0fe
SauravBanerjee04 <78271068+SauravBanerjee04@users.noreply.github.com>
2023-09-16 09:11:43 -0400

    changes

--- main.py
+++ main.py
@@ -11,28 +11,32 @@ FPS = 60
 CHAR_HEIGHT = 100
 CHAR_WIDTH = 70
 MAIN_CHARACTER = pygame.transform.scale(pygame.image.load(os.path.join('Assets','main_character.jpg')),(CHAR_HEIGHT,CHAR_WIDTH))
-BACKGROUND = pygame.transform.scale(pygame.image.load(os.path.join('Assets','map1.jpg')),(HEIGHT,WIDTH))
+BACKGROUND_MAP = pygame.transform.scale(pygame.image.load(os.path.join('Assets','map1.jpg')),(WIDTH,HEIGHT))
 
+loadingscreenset = True#Stars as false
+onmap = True # Starts as false
 
-# def draw_window():
-#     WIN.fill(BACKGROUND_COLOR)
-#     WIN.blit(MAIN_CHARACTER,(500,300))
-#     pygame.display.update()
-
-def draw_background():
+def draw_character():
     WIN.fill(BACKGROUND_COLOR)
     WIN.blit(MAIN_CHARACTER,(500,300))
     pygame.display.update()
 
+def draw_background():
+    WIN.fill(BACKGROUND_COLOR)
+    WIN.blit(BACKGROUND_MAP,(0,0))
+    pygame.display.update()
+
 def main():
     clock = pygame.time.Clock()
-    run = Truef
+    run = True
     while run:
         clock.tick(FPS)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
-        draw_window()
+        #draw_window()
+        if onmap:
+            draw_background()
     pygame.quit()
 
 if __name__ == "__main__":
