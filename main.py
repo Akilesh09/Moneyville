1790807e30e300589c194d5399c4b4ec67b5138f
Akilesh Mahesh <akilesh.mahesh07@gmail.com>
2023-09-16 16:37:13 -0400

    stat bar

--- main.py
+++ main.py
@@ -5,6 +5,15 @@ WIDTH, HEIGHT = 640, 512
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Moneyville")
 
+health = 100
+money = 1000
+social = 50
+
+# Define stat names
+health_name = "Health"
+money_name = "Money"
+social_name = "Social"
+
 BACKGROUND_COLOR = (255, 255, 255)
 FPS = 60
 
@@ -17,14 +26,43 @@ BACKGROUND_MAP = pygame.transform.scale(pygame.image.load(
 CHARACTER_X, CHARACTER_Y = 0, 0
 
 
-loadingscreenset = True  # Stars as false
+loadingscreenset = True  # Starts as false
 onmap = True  # Starts as false
 
+def draw_stats():
+    # Stat names and numerical values
+    pygame.draw.rect(WIN, (255, 255, 255), (0, 0, 150, 150))
 
+    
+    font = pygame.font.Font("font.ttf", 15)
+    
+    health_text = font.render(f"{health_name}: {health}", True, (0, 0, 0))
+    money_text = font.render(f"{money_name}: {money}", True, (0, 0, 0))
+    social_text = font.render(f"{social_name}: {social}", True, (0, 0, 0))
+
+
+    WIN.blit(health_text, (20, 20))
+    WIN.blit(money_text, (20, 60))
+    WIN.blit(social_text, (20, 100))
+
+    # Stat bars
+    pygame.draw.rect(WIN, (255, 0, 0), (20, 50, health, 10))
+    pygame.draw.rect(WIN, (0, 255, 0), (20, 90, money/1000, 10))
+    pygame.draw.rect(WIN, (0, 0, 255), (20, 130, social, 10))
+
+    # Stat bar outlines
+    pygame.draw.rect(WIN, (0, 0, 0), (20, 50, 100, 10), 2)
+    pygame.draw.rect(WIN, (0, 0, 0), (20, 90, 100, 10), 2)
+    pygame.draw.rect(WIN, (0, 0, 0), (20, 130, 100, 10), 2)
+
+    
 def draw(x,y):
     draw_background()
     if onmap:
         draw_character(x,y)
+    draw_stats()
+    pygame.display.update()
+
     #if onmap:
         #draw_character()
     #else: #draw options into four box screen
@@ -32,7 +70,6 @@ def draw(x,y):
 
 def draw_character(x, y):
     WIN.blit(MAIN_CHARACTER, (x, y))
-    pygame.display.update()
 
 
 def draw_background():  # draws the background, if we're on the map it draws the map otherwise it draws the boxes for the selection screen
@@ -71,6 +108,8 @@ def draw_background():  # draws the background, if we're on the map it draws the
 
 
 def main():
+    pygame.init()  # Initialize Pygame
+    pygame.font.init()  # Initialize the font module
     clock = pygame.time.Clock()
     run = True
     Coordinates = pygame.Rect(300,300,CHAR_WIDTH,CHAR_HEIGHT)
