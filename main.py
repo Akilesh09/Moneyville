e10d5d16f7ba7bb413ae38d1c98585504a42307d
Akilesh Mahesh <akilesh.mahesh07@gmail.com>
2023-09-17 02:00:11 -0400

    Add home screen

--- main.py
+++ main.py
@@ -30,7 +30,26 @@ counter = 0
 loadingscreenset = True  # Starts as false
 onmap = True  # Starts as false
 
+START_BUTTON_RECT = pygame.Rect(240, 200, 160, 50)
+EXIT_BUTTON_RECT = pygame.Rect(240, 300, 160, 50)
 
+def draw_home_screen():
+    WIN.fill(BACKGROUND_COLOR)
+
+    # Draw the "Start" button
+    pygame.draw.rect(WIN, (0, 255, 0), START_BUTTON_RECT)
+    start_font = pygame.font.Font(None, 36)
+    start_text = start_font.render("Start", True, (0, 0, 0))
+    start_text_rect = start_text.get_rect(center=START_BUTTON_RECT.center)
+    WIN.blit(start_text, start_text_rect)
+
+    # Draw the "Exit" button
+    pygame.draw.rect(WIN, (255, 0, 0), EXIT_BUTTON_RECT)
+    exit_font = pygame.font.Font(None, 36)
+    exit_text = exit_font.render("Exit", True, (0, 0, 0))
+    exit_text_rect = exit_text.get_rect(center=EXIT_BUTTON_RECT.center)
+    WIN.blit(exit_text, exit_text_rect)
+    pygame.display.update()
 
 
 def draw_stats():
@@ -108,23 +127,40 @@ def main():
     pygame.font.init()  # Initialize the font module
     clock = pygame.time.Clock()
     run = True
+    in_home_screen = True
     Coordinates = pygame.Rect(300,300,CHAR_WIDTH,CHAR_HEIGHT)
     while run:
         clock.tick(FPS)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
-        keys = pygame.key.get_pressed()
-        # print(Coordinates.x, Coordinates.y)
-        if keys[pygame.K_LEFT] and Coordinates.x>-49:
-            Coordinates.x -= 3
-        if keys[pygame.K_RIGHT] and Coordinates.x < 538:
-            Coordinates.x += 3
-        if keys[pygame.K_UP] and Coordinates.y > -3:
-            Coordinates.y -= 3
-        if keys[pygame.K_DOWN] and Coordinates.y < 423:
-            Coordinates.y += 3
-        draw(Coordinates.x,Coordinates.y)
+        
+        if in_home_screen:
+            # Draw the home screen
+            draw_home_screen()
+
+            # Check for button clicks
+            mouse_pos = pygame.mouse.get_pos()
+            if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+                if START_BUTTON_RECT.collidepoint(mouse_pos):
+                    # Start button clicked, transition to the game
+                    in_home_screen = False
+                    # Add your game initialization code here
+                elif EXIT_BUTTON_RECT.collidepoint(mouse_pos):
+                    # Exit button clicked, quit the game
+                    pygame.quit()
+        else:
+            keys = pygame.key.get_pressed()
+            # print(Coordinates.x, Coordinates.y)
+            if keys[pygame.K_LEFT] and Coordinates.x>-49:
+                Coordinates.x -= 3
+            if keys[pygame.K_RIGHT] and Coordinates.x < 538:
+                Coordinates.x += 3
+            if keys[pygame.K_UP] and Coordinates.y > -3:
+                Coordinates.y -= 3
+            if keys[pygame.K_DOWN] and Coordinates.y < 423:
+                Coordinates.y += 3
+            draw(Coordinates.x,Coordinates.y)
     pygame.quit()
 
 
@@ -157,7 +193,7 @@ def choicemenu(prompt,choices,consequence):
             consequence[4]
             return
         
-def gym():
+#def gym():
     choicemenu("Choose a workout", ["30 minute run", "Weightlifting (1 hour)", "Return Back"], )
 def bank():
     choicemenu("What type of fund would you like to invest in?", ["Stocks - Medium risk with average ROI of 10% (1 hour)", "Bonds - Low risk with average ROI of 4%(1 hour)", "Certified Deposit(CD) - Very low risk with average ROI of 2%(1 hour)","EXIT"])
